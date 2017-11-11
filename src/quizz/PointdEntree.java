99d43399d9a0b44397674067ef4d4adf37a1d442
Formation <Formation@10.18.0.79>
2017-11-11 11:32:11 +0100

    - v0.1

---
+++ src/quizz/PointdEntree.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizz;
+
+/**
+ *
+ * @author Formation
+ */
+public class PointdEntree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
